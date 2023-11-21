2421929ee3820c7abb7afc82b0805bc3a9d69913
hamymah <hanymah823@gmail.com>
2023-11-21 20:44:19 +0200

    add the componets of form and inputs and add them to my screen ManageExpense to add a new expense

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
 import Button from "../components/UI/Button";
+import ExpenseForm from "../components/manageExpense/ExpenseForm";
 
 function ManageExpense({ route, navigation }) {
   const expenseId = route.params?.expenseId;
@@ -27,10 +28,14 @@ function ManageExpense({ route, navigation }) {
   return (
     <View style={styles.container}>
       <View style={styles.btnsContainer}>
-        <Button style={styles.button} onPress={cancelHandler} mode="flat">Cancel</Button>
-        <Button style={styles.button} onPress={updateHandler}>{expenseIdIsExisting ? 'Update' : 'Add'}</Button>
+        <Button style={styles.button} onPress={cancelHandler} mode="flat">
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={updateHandler}>
+          {expenseIdIsExisting ? "Update" : "Add"}
+        </Button>
       </View>
-
+      <ExpenseForm />
       <View style={styles.deleteExpense}>
         {expenseIdIsExisting && (
           <IconButton
@@ -47,27 +52,27 @@ function ManageExpense({ route, navigation }) {
 
 export default ManageExpense;
 
-const styles=StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor:GlobalStyles.colors.primary800,
-    padding:24
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: GlobalStyles.colors.primary800,
+    padding: 24,
   },
-  deleteExpense:{
-    alignItems:'center',
-    borderTopWidth:2,
-    borderTopColor:GlobalStyles.colors.primary200,
-    marginTop:16,
-    paddingTop:8,
+  deleteExpense: {
+    alignItems: "center",
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    marginTop: 16,
+    paddingTop: 8,
   },
-  button:{
-    marginHorizontal:8,
-    minWidth:120
+  button: {
+    marginHorizontal: 8,
+    minWidth: 120,
   },
-  btnsContainer:{
-    flexDirection:'row',
-    alignItems:'center',
-    justifyContent:"center",
+  btnsContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
     // gap:8
-  }
-})
+  },
+});
